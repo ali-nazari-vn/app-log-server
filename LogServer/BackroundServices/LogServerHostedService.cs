b3546d4d74d91204f756e999e1e97f9c3926ec90
numx <barga6267@gmail.com>
2024-10-11 17:53:48 +0330

    init

---
+++ LogServer/BackroundServices/LogServerHostedService.cs
@@ -0,0 +1,45 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Serilog;
+using System.Text;
+
+namespace LogServer.BackroundServices
+{
+    public class LogServerHostedService : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var logQueueName = "log-queu";
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
+            var connection = factory.CreateConnection();
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: logQueueName,
+                                   durable: false,
+                                   exclusive: false,
+                                   autoDelete: false,
+                                   arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    IDictionary<string, object> headers = ea.BasicProperties.Headers; // get headers from Received msg
+
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body.ToArray());
+                    Console.WriteLine(message);
+
+                    Log.Error($"Error {DateTime.Now}:  -  " + message);
+                };
+
+                channel.BasicConsume(queue: logQueueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ LogServer/Program.cs
@@ -0,0 +1,36 @@
+using LogServer.BackroundServices;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<LogServerHostedService>();
+
+#region Logs
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+Log.Information("start application");
+
+#endregion
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
